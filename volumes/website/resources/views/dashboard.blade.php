c41b89a7d6a035dec06bec50f753cb579ce2e2fe
tchristerus <tchristerus@outlook.com>
2017-02-08 21:22:56 +0100

    Creates tabs for team projects

--- volumes/website/resources/views/dashboard.blade.php
+++ volumes/website/resources/views/dashboard.blade.php
@@ -15,7 +15,7 @@
 </head>
 <body>
 <div class="container-fluid">
-    <nav class="navbar navbar-default">
+    <nav class="navbar navbar-default navbar-no-margin-bottom">
         <div class="container-fluid">
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
@@ -57,24 +57,38 @@
 
 
     <div class="row">
-        <div class="col-md-12">
-            @foreach($projects as $project)
-                <div class="col-md-4">
-                    <div class="panel panel-success">
-                        <div class="panel-heading">
-                            <h3 class="panel-title">{{$project->name}}</h3>
-                            <a href="" data-title="{{$project->name}}" data-href="dashboard/project/remove/{{$project->id}}" data-toggle="modal" data-target="#remove-project-dialog"><i class="material-icons delete-button">clear</i></a>
-                        </div>
-                        <div class="panel-body">
-                            <strong>Description: </strong>
-                            <p>{{$project->description}}</p>
-                            <i class="fa fa-clock-o fa-1x vcenter"></i><strong> 200 hours total</strong><br>
-                            <i class="fa fa-hourglass-start fa-1x vcenter"></i> Not active
-                            <a class="btn btn-raised btn-info btn-block">Start<div class="ripple-container"></div></a>
-                        </div>
+        <div class="col-md-12 no-padding">
+            <div class="bs-component">
+                <ul class="nav nav-tabs" style="margin-bottom: 15px;">
+                    <li class="active"><a href="#home" data-toggle="tab" aria-expanded="true">My own projects<div class="ripple-container"></div></a></li>
+                    <li><a href="#profile" data-toggle="tab" aria-expanded="false">Group projects<div class="ripple-container"></div></a></li>
+                </ul>
+                <div id="myTabContent" class="tab-content">
+                    <div class="tab-pane fade active in" id="home">
+                        @foreach($projects as $project)
+                            <div class="col-md-4">
+                                <div class="panel panel-success">
+                                    <div class="panel-heading">
+                                        <h3 class="panel-title">{{$project->name}}</h3>
+                                        <a href="" data-title="{{$project->name}}" data-href="dashboard/project/remove/{{$project->id}}" data-toggle="modal" data-target="#remove-project-dialog"><i class="material-icons delete-button">clear</i></a>
+                                    </div>
+                                    <div class="panel-body">
+                                        <strong>Description: </strong>
+                                        <p>{{$project->description}}</p>
+                                        <i class="fa fa-clock-o fa-1x vcenter"></i><strong> 200 hours total</strong><br>
+                                        <i class="fa fa-hourglass-start fa-1x vcenter"></i> Not active
+                                        <a class="btn btn-raised btn-info btn-block">Start<div class="ripple-container"></div></a>
+                                    </div>
+                                </div>
+                            </div>
+                        @endforeach
+                     </div>
+                    <div class="tab-pane fade" id="profile">
+                        <p>Team projects</p>
                     </div>
                 </div>
-            @endforeach
+                <div id="source-button" class="btn btn-primary btn-xs" style="display: none;">&lt; &gt;</div>
+            </div>
         </div>
     </div>
 </div>
